3a46a0ec51b3432fe4845cc0ccf7dd59e31c7db2
HarryBour <2466262@cegepmontpetit.ca>
2026-09-07 13:12:16 -0400

    ListeSimple commentaire à réviser

--- src/main/kotlin/ListeSimple.kt
+++ src/main/kotlin/ListeSimple.kt
@@ -6,11 +6,13 @@ println(repete(4,2))
     println(repete(3,4))
 }
 fun  repete(n: Int, nombreFois: Int) : List<Int> {
-   val result = mutableListOf<Int>()
+   val result = mutableListOf<Int>() // crée la variable résultat et le déclare comme une listemutable de nombre entiers
 
-    for (nombre in 1..n) {
-        for(repetition in 1..nombreFois) {
-            result.add(repetition)
+    for (nombre in 1..n) // nombre répété à la liste
+    {
+        for(repetition in 1..nombreFois) // nombre ajouté à la liste
+        {
+            result.add(repetition) // répete et ajoute le nombre ajouté
         }
     }
     return result
